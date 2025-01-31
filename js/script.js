c95947d7ca10b10ebdddef205ecc6cfb61a93289
ARAUJO José <jose.araujo-firmino-de-lima@etu.univ-grenoble-alpes.fr>
2025-01-31 22:52:58 +0100

    refactorisation du code et ajout des pictogrammes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
 
 
-let lastScrollTop = 0; // Última posição do scroll
-let scrollThreshold = 20; // Limite de rolagem para esconder a navbar
-let scrollDownCount = 0; // Contador de roladas para baixo
+let lastScrollTop = 0;
+let scrollThreshold = 20;
+let scrollDownCount = 0;
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", () => {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrollTop > lastScrollTop && (scrollTop - lastScrollTop) > scrollThreshold) {
-        // Rolando para baixo com diferença maior que o limite definido
         scrollDownCount++;
         if (scrollDownCount >= 1) {
-            navbar.classList.add("hidden"); // Esconde a navbar
+            navbar.classList.add("hidden");
         }
     } else if (scrollTop < lastScrollTop) {
-        // Rolando para cima
-        navbar.classList.remove("hidden"); // Mostra a navbar
-        scrollDownCount = 0; // Reinicia o contador
-    }
 
-    lastScrollTop = scrollTop; // Atualiza a última posição do scroll
+        navbar.classList.remove("hidden");
+        scrollDownCount = 0; 
+    }
+    lastScrollTop = scrollTop;
 });
